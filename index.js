894bab7580b1c776cd92be00bf3c7cd0b23bde30
Anastasiya Danilova <danilovaness@yandex.ru>
2023-04-23 22:13:59 +0300

    Feat: add platforms and scroll the background

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const canvas = document.querySelector('canvas')
+const canvas = document.querySelector('.canvas')
 
 canvas.width = innerWidth
 canvas.height = innerHeight
-
 
 const c = canvas.getContext('2d')
 
@@ -41,7 +40,28 @@ class Player {
   }
 }
 
+class Platform {
+  constructor({x, y}) {
+    this.position = {
+      x,
+      y
+    }
+    this.width = 200
+    this.height = 20
+  }
+
+  draw() {
+    c.fillStyle = 'blue'
+    c.fillRect(this.position.x, this.position.y,
+      this.width, this.height
+    )
+  }
+}
+
 const player = new Player()
+const platforms = [new Platform({x: 200, y: 300}), new Platform({x: 400, y: 500})]
+
+
 const keys = {
   right: {
     pressed: false
@@ -56,19 +76,50 @@ function animate() {
 
   c.clearRect(0, 0, canvas.width, canvas.height)
   player.update()
-  if (keys.right.pressed) {
-    player.velocity.x = 5 
-  }
-  else if (keys.left.pressed) {
-    player.velocity.x = -5 
-  }
-  else player.velocity.x = 0 
+  platforms.forEach(platform => {
+    platform.draw()
+  })
 
+
+  // player move
+  if (keys.right.pressed
+    && player.position.x < 400) {
+    player.velocity.x = 5
+  }
+  else if (keys.left.pressed && player.position.x > 100) {
+    player.velocity.x = -5
+  }
+  else {
+    player.velocity.x = 0
+  
+    if (keys.right.pressed) {
+      platforms.forEach(platform => {
+        platform.position.x -= 5
+      })
+    }
+    else if (keys.left.pressed) {
+      platforms.forEach(platform => {
+        platform.position.x += 5
+      })
+    }
+  }
+
+  // platform collision detection
+  platforms.forEach(platform => {
+  if (
+    player.position.y + player.height <= platform.position.y
+    && player.position.y + player.height + player.velocity.y >= platform.position.y
+    && player.position.x + player.width >= platform.position.x
+    && player.position.x <= platform.position.x + platform.width
+  ) {
+    player.velocity.y = 0
+  }
+})
 }
 animate()
 
 addEventListener('keydown', ({ keyCode }) => {
-  console.log(keyCode)
+
   switch (keyCode) {
     // left
     case 65:
@@ -93,11 +144,9 @@ addEventListener('keydown', ({ keyCode }) => {
       player.velocity.y -= 20;
       break
   }
-  console.log(keys.right.pressed)
 })
 
 addEventListener('keyup', ({ keyCode }) => {
-  console.log(keyCode)
   switch (keyCode) {
     // left
     case 65:
@@ -122,6 +171,5 @@ addEventListener('keyup', ({ keyCode }) => {
       // player.velocity.y -= 20;
       break
   }
-  console.log(keys.right.pressed)
 
 })
